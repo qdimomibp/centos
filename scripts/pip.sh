29cea46859ba807b596d3733d355a90cd0eecb95
qdimomibp <qdimomibp@naver.com>
2019-02-12 10:48:10 +0900

    setuptools 주석 추가

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -3,6 +3,7 @@ cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip-py"
 python get-pip.py
 
+#pip install --upgrade setuptools
 pip install --user pymediainfo
 pip install --user PySide2
 pip install --user numpy
